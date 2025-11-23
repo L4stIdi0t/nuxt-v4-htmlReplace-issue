0e8b488ae9a360da7b9b61ce15498d7594482811
L4stIdi0t <75753024+L4stIdi0t@users.noreply.github.com>
2025-11-23 17:22:17 +0100

    Add files via upload

---
+++ app/pages/index.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="container p-8">
+    <div>
+      <span class="bg-red-500">
+        lalala
+      </span>
+      It works!
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
